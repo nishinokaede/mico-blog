719a088208aa4a790070bc2ea06b8978bb49bdb9
densu <nishinokaede116@gmail.com>
2026-06-17 03:37:18 +0800

    # fix vite allowd host

--- micro-blog/vite.config.ts
+++ micro-blog/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: ['.densu.cc'],
     proxy: {
       '/api': {
         target: process.env.VITE_API_TARGET || 'http://127.0.0.1:8005',
